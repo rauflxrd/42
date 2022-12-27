f72e9bd2e10d567a82ede388b56e5a20ad38a132
[rauf] <[raufam@code.edu.az]>
2022-12-27 12:54:10 +0400

    init

---
+++ hw 2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace hw_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            if (n > 9999 && n < 100000)
+            {
+                Console.Write($"{4}{n}{2}");
+            }
+            else
+            {
+                Console.WriteLine("5 reqemli eded deyil");
+            }
+
+
+        }
+    }
+}
